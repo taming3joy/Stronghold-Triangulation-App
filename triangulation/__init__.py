3a2337653661526f3b43f20e3892535b757d01f0
Thanawat Kositjaroenkul <thanawat.kositjaroenkul@gmail.com>
2025-06-26 15:38:10 +0700

    Use OOP paradigm for gui

---
+++ triangulation/__init__.py
@@ -0,0 +1,1 @@
+from .t_gui import triangulation_gui
--- triangulation/t_gui.py
+++ triangulation/t_gui.py
@@ -1,43 +1,41 @@
 import tkinter as tk
 from .t_math import triangulate
 
-def run_app():
-    root = tk.Tk()
-    root.title("Triangulation")
-    root.geometry("300x135")
+class triangulation_gui():
+    def __init__(self):
+        
+        self.root = tk.Tk()
+        self.root.title("Stronghold Triangulation")
+        self.root.geometry("450x200")
 
+        self.heading = tk.Label(self.root,text="Format: X-coordinate Z-coordinate Angle",font=('Arial',15))
+        self.heading.pack(pady=10)
 
-    def on_click():
+        self.entry1 = tk.Entry(self.root,font=("Arial",12))
+        self.entry1.pack()
+
+        self.entry2 = tk.Entry(self.root,font=("Arial",12))
+        self.entry2.pack(pady=5)
+
+        self.button = tk.Button(self.root,text="Find coordinates",command=self.on_click,font=("Arial",12))
+        self.button.pack(pady=5)
+
+        self.overworld = tk.Label(self.root,text="",font=("Arial",12))
+        self.overworld.pack()
+
+        self.nether = tk.Label(self.root,text="",font=("Arial",12))
+        self.nether.pack()
+
+        self.root.mainloop()
+
+    def on_click(self):
         try:
-            measurement1=[float(x) for x in str(entry1.get()).split(" ")]
-            measurement2=[float(x) for x in str(entry2.get()).split(" ")]
+            measurement1=[float(x) for x in str(self.entry1.get()).split(" ")]
+            measurement2=[float(x) for x in str(self.entry2.get()).split(" ")]
             stronghold_coordinates_overworld = triangulate(measurement1,measurement2)
             stronghold_coordinates_nether = tuple([x//8 for x in stronghold_coordinates_overworld])
-            overworld.config(text = f"Overworld = {stronghold_coordinates_overworld}" )
-            nether.config(text=f"Nether = {stronghold_coordinates_nether}")
+            self.overworld.config(text = f"Overworld = {stronghold_coordinates_overworld}" )
+            self.nether.config(text=f"Nether = {stronghold_coordinates_nether}")
         except:
-            overworld.config(text="Invalid input(s)")
-            nether.config(text="")
-
-    Heading = tk.Label(root,text="Format: X-coordinate Z-coordinate Angle")
-    entry1 = tk.Entry(root)
-    entry2 = tk.Entry(root)
-
-    button = tk.Button(root,text="Find coordinates",command=on_click)
-    overworld = tk.Label(root,text="")
-    nether = tk.Label(root,text="")
-
-
-
-    #structure
-    Heading.pack()
-    entry1.pack()
-    entry2.pack()
-    button.pack()
-    overworld.pack()
-    nether.pack()
-
-    root.mainloop()
-
-#Add use of F3+C
-#Add distance to stronghold
+            self.overworld.config(text="Invalid input(s)")
+            self.nether.config(text="")
